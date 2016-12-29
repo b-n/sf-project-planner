e9a7c64352e2f5af23830d9415cb7cd17ac59f7c
Leo Alves <leoalves@users-Mac.local>
2016-12-29 11:51:19 +0100

    add logo to header

--- web/src/layout/header.js
+++ web/src/layout/header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
 
 const Header = () => {
   return (
-    <header>
-      This is the Header
+    <header className='slds-global-header_container'>
+      <div className='slds-global-header slds-grid slds-grid--align-spread'>
+        <div className='slds-global-header__item'>
+          <div className='slds-global-header__logo'>
+            <img src={Logo} alt='logo'/>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
