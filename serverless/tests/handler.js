416123c27d8247b08a79428ae7dd1d3fa2f4f6a7
Rebecca Hill <beerecca@gmail.com>
2017-01-07 14:36:57 +0100

    Authorizer function

--- serverless/tests/handler.js
+++ serverless/tests/handler.js
@@ -1,17 +1,31 @@
-import { _login } from '../handler.js';
+import { _login, _authorizer } from '../handler.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// (() => {
+//     const event = {
+//         body: {
+//             username: process.env.USERNAME,
+//             password: process.env.PASSWORD
+//         }
+//     }
+//
+//     _login(event, {} , (error, success) => {
+//         console.log('error', error);
+//         console.log('success', success);
+//     });
+// })();
+
+
 (() => {
     const event = {
-        body: { 
-            username: process.env.USERNAME,
-            password: process.env.PASSWORD
-        }
-    }
+        type: 'TOKEN',
+        authorizationToken: 'Bearer token',
+        methodArn: 'arn:aws:execute-api:us-east-1:411615132843:zo5571jwvj/dev/GET/resources'
+    };
 
-    _login(event, {} , (error, success) => {
-        console.log('error', error);
-        console.log('success', success);
+    _authorizer(event, {} , (error, success) => {
+        if (error) console.log('error', error);
+        if (success) console.log('success', success);
     });
 })();
