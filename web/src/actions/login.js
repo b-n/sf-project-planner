b937f961a8ca04b50cf28bdb69ecd9da3ee5f295
Leo Alves <leoalves@users-Mac.local>
2017-01-09 21:13:34 +0100

    start the refactor to use sagas instead of redux thunk

--- web/src/actions/login.js
+++ web/src/actions/login.js
@@ -1,8 +1,36 @@
 import actionTypes from './action-types'
 
-export function loginAttempt(username, password) {
+
+const loginAttempt = () => {
   return {
     type: actionTypes.LOGIN_ATTEMPT,
-    payload: true
+    payload: {
+      loggedIn: false,
+      shouldRedirect: false,
+      displayError: true,
+      displaySpinner: true
+    }
+  }
+}
+
+const loginSucceded = () => {
+  return {
+    type: actionTypes.LOGIN_REDIRECT,
+    payload: {
+      loggedIn: true,
+      shouldRedirect: true,
+      displayError: false,
+      displaySpinner: false
+    }
+  }
+}
+
+const loginError = () => {
+  type: action.Types.LOGIN_ERROR,
+  payload: {
+    loggedIn: false,
+    shouldRedirect: false,
+    displayError: true,
+    displaySpinner: false
   }
 }
--- web/src/reducers/login-reducer.js
+++ web/src/reducers/login-reducer.js
@@ -1,15 +1,22 @@
 import actionTypes from '../actions/action-types'
 
 const initialState = {
-  loggedIn: false
+  loggedIn: false,
+  shouldRedirect: false,
+  displayError: false,
+  displaySpinner: false
 }
 
 const LoginReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.LOGIN_ATTEMPT:
-      return {
-        loggedIn: action.payload
-      }
+      return Object.assign({}, state, {
+        loggedIn: true
+      })
+    case actionTypes.LOGIN_REDIRECT:
+      return Object.assign({}, state, {
+        shouldRedirect: true
+      })
     default:
       return state
   }
