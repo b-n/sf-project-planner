020058924f6ca2b0d9efd254e98dd49caa73fcaf
Ben Naylor <ben.naylor@beethree.nl>
2017-01-18 21:06:06 +0100

    refactor saga to have api in another file

---
+++ web/src/lib/api.js
@@ -0,0 +1,33 @@
+import endpoints from '../endpoints'
+
+export function doLogin(username, password) {
+    
+    if (!username || !password) return Promise.reject(new Error('Need a username and password'))
+
+    return new Promise((resolve, reject) => {
+        
+        const body = JSON.stringify({
+            username,
+            password
+        })
+
+        fetch(endpoints.login, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (!data.bearerToken) {
+                reject(new Error('Wrong username/password'))
+                return;
+            } 
+
+            resolve(data);
+        })
+        .catch(e => {
+            reject(new Error('Error trying to login'))
+        })
+
+    })
+}
--- web/src/sagas/login-saga.js
+++ web/src/sagas/login-saga.js
@@ -1,31 +1,23 @@
 import { put, takeEvery } from 'redux-saga/effects'
 
-import endpoints from '../endpoints'
+import * as api from '../lib/api.js'
 import actionTypes from '../actions/action-types'
 
 function* attemptLogin(action){
   try {
     const { username, password } = action.payload
 
-    const body = JSON.stringify({
-        username,
-        password
-    })
+    const data = yield api.doLogin(username, password);
 
-    const data = yield fetch(endpoints.login, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body
-    })
-    .then(response => response.json())
-
-    if (!data.bearerToken) throw new Error('no token in response')
     yield put({ type: actionTypes.LOGIN_REDIRECT, payload: {
         token: data.bearerToken
       }
     })
   } catch(e){
-    yield put({ type: actionTypes.LOGIN_ERROR })
+    yield put({ type: actionTypes.LOGIN_ERROR, payload: {
+        message: e.message
+      }
+    })
   }
 }
 
