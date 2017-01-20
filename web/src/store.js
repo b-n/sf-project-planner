ff4065f2d117a3532d6fdd4a728be1218d237598
Ben Naylor <ben.naylor@beethree.nl>
2017-01-20 21:20:53 +0100

    because redux dev tools are the best

--- web/src/store.js
+++ web/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import 'babel-polyfill'
 
 import logger from 'redux-logger'
@@ -7,8 +7,11 @@ import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers/root-reducer'
 import rootSaga from './sagas/root-saga'
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger()))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware, logger())))
 
 sagaMiddleware.run(rootSaga)
 
