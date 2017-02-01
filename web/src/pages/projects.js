964f1d58b19cefd12a0df2663376484697a5174e
Leo Alves <leoalves@users-Mac.local>
2017-02-01 19:31:46 +0100

    redirect if not logged in

--- web/src/pages/projects.js
+++ web/src/pages/projects.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
 import ProjectTable from '../components/project-table'
 
-const Projects = () => {
-  return (
-    <div className='slds-container--center slds-container--x-large'>
-      <ProjectTable />
-    </div>
-  )
+
+// TODO - turn project page into a more generic view component after login
+class Projects extends Component {
+
+  componentDidMount() {
+    if (!this.props.loggedIn) this.context.router.push('/')
+  }
+
+  render() {
+    return (
+      <div className='slds-container--center slds-container--x-large'>
+        <ProjectTable />
+      </div>
+    )
+  }
 }
 
+Projects.contextTypes = {
+  router: PropTypes.object.isRequired
+}
 
-export default Projects
+const mapStateToProps = (state) =>  {
+  return {
+    loggedIn: state.login.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(Projects)
