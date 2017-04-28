539767b6e42bb5592e9d20bbe8f9b7d657220093
Ben Naylor <ben.naylor@beethree.nl>
2017-04-28 10:54:00 +0200

    more .js extensions!

--- serverless/tests/handler.js
+++ serverless/tests/handler.js
@@ -1,21 +1,4 @@
 import { _login, _authorizer, _projects, _resources } from '../handler.js';
-import dotenv from 'dotenv';
-dotenv.config();
-
-// (() => {
-//     const event = {
-//         body: {
-//             username: process.env.USERNAME,
-//             password: process.env.PASSWORD
-//         }
-//     }
-//
-//     _login(event, {} , (error, success) => {
-//         console.log('error', error);
-//         console.log('success', success);
-//     });
-// })();
-
 
 (() => {
     const event = {
